7cdf7cdece817aca7170b2a81bba2a0c65cdb7db
yejin <yj040102@naver.com>
2024-07-25 18:01:51 +0900

    first setting

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <Browserrouter>
+      <div className="app">
+        <Routes>
+          
+        </Routes>
+      </div>
+    </Browserrouter>
+  )
+}
+
+export default App
