c75428e29e263e69d622f6e07f4b8fe3cf3c8a1c
juuuuone <jjw9597@gmail.com>
2024-08-06 17:43:29 +0900

    FEAT : 사이드바 구현, 로그인 화면 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import Login from "./components/Login.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RedirectPage from "./components/RedirectPage.jsx";
-import './App.css'
-import WinRate from './components/WinRate'
+import "./App.css";
+import WinRate from "./components/WinRate";
+import Sidebar from "./components/Sidebar.jsx";
 
 function App() {
   return (
     <BrowserRouter>
+      <Sidebar />
       <Routes>
         <Route path="/login" exact element={<Login />} />
         <Route path="/oauth/callback/kakao" element={<RedirectPage />} />
-        <Route path="/winrate" element={<WinRate/>}/>
+        <Route path="/winrate" element={<WinRate />} />
       </Routes>
     </BrowserRouter>
   );
- 
-
 }
 
 export default App;
