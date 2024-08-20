a8d8169912a4d07aa66252197db2b1377ee74130
bini0918 <165636716+bini0918@users.noreply.github.com>
2024-08-21 01:56:19 +0900

    Update StickerScreen.jsx

--- src/components/StickerScreen.jsx
+++ src/components/StickerScreen.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import sticker1 from '../assets/Mask group.svg';
+import sticker2 from '../assets/Mask group (1).svg';
+import sticker3 from '../assets/Mask group (2).svg';
+import sticker4 from '../assets/Mask group (3).svg';
+import sticker5 from '../assets/Mask group (4).svg';
+import sticker6 from '../assets/Mask group (5).svg';
+import sticker7 from '../assets/Mask group (6).svg';
+import sticker8 from '../assets/Mask group (7).svg';
+import sticker9 from '../assets/Mask group (8).svg';
 import '../styles/StickerScreen.css'; 
 
 const StickerScreen = ({ selectedDate }) => {
@@ -16,39 +25,39 @@ const StickerScreen = ({ selectedDate }) => {
     <div className="sticker-screen">
       <div className="stickers">
         <div className="sticker" onClick={() => handleStickerClick(1)}>
-          <img src="/Mask group.svg" alt="스티커 1" />
+         <img src={sticker1} alt="스티커 1" />
           <div className="sticker-text">완전 최고</div>
         </div>
         <div className="sticker" onClick={() => handleStickerClick(2)}>
-          <img src="/Mask group (1).svg" alt="스티커 2" />
+        <img src={sticker2} alt="스티커 2" />
           <div className="sticker-text">우승가자</div>
         </div>
         <div className="sticker" onClick={() => handleStickerClick(3)}>
-          <img src="/Mask group (2).svg" alt="스티커 3" />
+        <img src={sticker3} alt="스티커 3" />
           <div className="sticker-text">폼미쳤다</div>
         </div>
         <div className="sticker" onClick={() => handleStickerClick(4)}>
-          <img src="/Mask group (3).svg" alt="스티커 4" />
+        <img src={sticker4} alt="스티커 4" />
           <div className="sticker-text">스윕좋다</div>
         </div>
         <div className="sticker" onClick={() => handleStickerClick(5)}>
-          <img src="/Mask group (4).svg" alt="스티커 5" />
+        <img src={sticker5} alt="스티커 5" />
           <div className="sticker-text">우천취소</div>
         </div>
         <div className="sticker" onClick={() => handleStickerClick(6)}>
-          <img src="/Mask group (5).svg" alt="스티커 6" />
+        <img src={sticker6} alt="스티커 6" />
           <div className="sticker-text">연패그만</div>
         </div>
         <div className="sticker" onClick={() => handleStickerClick(7)}>
-          <img src="/Mask group (6).svg" alt="스티커 7" />
+        <img src={sticker7} alt="스티커 7" />
           <div className="sticker-text">특타하자</div>
         </div>
         <div className="sticker" onClick={() => handleStickerClick(8)}>
-          <img src="/Mask group (7).svg" alt="스티커 8" />
+        <img src={sticker8} alt="스티커 8" />
           <div className="sticker-text">펑고하자</div>
         </div>
         <div className="sticker" onClick={() => handleStickerClick(9)}>
-          <img src="/Mask group (8).svg" alt="스티커 9" />
+        <img src={sticker9} alt="스티커 9" />
           <div className="sticker-text">반성하자</div>
         </div>
       </div>
