dc1704b9642005633df9a703b822da05c6e05443
yejin <yj040102@naver.com>
2024-08-21 23:16:38 +0900

    first loading

--- src/components/Lottie.jsx
+++ src/components/Lottie.jsx
@@ -10,7 +10,7 @@ function LoadingScreen() {
     // 페이지 이동을 위한 타이머 설정
     const timer = setTimeout(() => {
       navigate('/login'); // 애니메이션이 끝난 후 이동할 페이지
-    }, 4000); // 애니메이션의 지속 시간에 맞게 설정
+    }, 5000); // 애니메이션의 지속 시간에 맞게 설정
 
     return () => clearTimeout(timer); // 컴포넌트가 언마운트 될 때 타이머 정리
   }, [navigate]);
