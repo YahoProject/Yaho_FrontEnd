ebef07faec4825ed4c034309d8241b07a60c578a
bini0918 <165636716+bini0918@users.noreply.github.com>
2024-08-21 19:12:36 +0900

    Update Calendar.jsx

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -7,37 +7,41 @@ import { useNavigate } from 'react-router-dom';
 
 const Calendar = () => {
   const [activeButton, setActiveButton] = useState('diary'); // 현재 활성화된 버튼 상태
-  const nav=useNavigate()
+  const nav = useNavigate();
+
   const handleButtonClick = (buttonType) => {
     setActiveButton(buttonType);
-
-
-    };
-    
+  };
 
   return (
-    <div className="heeder">
+    <div className="calendar-container">
       <p className="title">야구일기</p>
 
       <div className="button-container">
-        <button 
-          className={`cal-button ${activeButton === 'diary' ? 'active' : ''}`} 
+        <button
+          className={`cal-button ${activeButton === 'diary' ? 'active' : ''}`}
           onClick={() => handleButtonClick('diary')}
         >
           야구 일기
         </button>
-        <button 
-          className={`cal-button ${activeButton === 'stats' ? 'active' : ''}`} 
+        <button
+          className={`cal-button ${activeButton === 'stats' ? 'active' : ''}`}
           onClick={() => handleButtonClick('stats')}
         >
           승률 계산
         </button>
       </div>
 
-      {activeButton === 'diary' && <><CustomCalendar /><TodayGame /></>}
-      {activeButton === 'stats' && <WinRate />}
+      <div className="calendar-content">
+        {activeButton === 'diary' && (
+          <>
+            <CustomCalendar />
+            <TodayGame />
+          </>
+        )}
+        {activeButton === 'stats' && <WinRate />}
+      </div>
     </div>
-
   );
 };
 
